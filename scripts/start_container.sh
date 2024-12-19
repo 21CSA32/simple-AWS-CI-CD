0fbb33ccf086c3a3ed7a45909f4f5b0d0cca1188
Nithish Kumar. <88938025+Nithish1641@users.noreply.github.com>
2024-12-19 17:04:06 +0530

    new

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull nithish164/simple-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 nithish164/simple-python-app
