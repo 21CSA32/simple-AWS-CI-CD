ca45857fd761dbf00e21f38b1e5a03b869d5235f
Nithish Kumar. <88938025+Nithish1641@users.noreply.github.com>
2024-12-20 19:38:19 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,11 @@
 set -e
 
 # Stop the running container (if any)
-containerid='docker ps | awk -F " " '{print $1}''
-docker rm -f $containerid
-echo "Hi, I am going to stop"
+containerid=$(docker ps | awk '{print $1}')
+
+if [ -n "$containerid" ]; then
+    docker rm -f "$containerid"
+    echo "Hi, I am going to stop the container: $containerid"
+else
+    echo "No running container found."
+fi
