b7032e8422e22aab6bb6a9d8005104855d7682a3
Nithish Kumar. <88938025+Nithish1641@users.noreply.github.com>
2024-12-20 19:09:15 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull nithish164/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 9000:9000 nithish164/simple-python-flask-app:latest
+docker run -d -p 5000:9000 nithish164/simple-python-flask-app:latest
