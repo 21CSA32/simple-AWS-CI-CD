60707dfbee429ba4bd5f7fe4ee0810f057ea7932
Nithish Kumar. <88938025+Nithish1641@users.noreply.github.com>
2024-12-20 19:47:24 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Stop the running container (if any)
-containerid=$(docker ps | awk '{print $1}')
-
+containerid=$(docker ps -q) # Use -q to only get container IDs
 if [ -n "$containerid" ]; then
     docker rm -f "$containerid"
     echo "Hi, I am going to stop the container: $containerid"
